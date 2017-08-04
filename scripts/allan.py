26a5b389ce87deaf4af4f2faf3583c1ce3bab9fd
Dan Pierce <jdp0009@auburn.edu>
2017-08-04 11:24:48 -0500

    fixed issue with delta measurements and added plotting

--- scripts/allan.py
+++ scripts/allan.py
@@ -6,6 +6,7 @@ import numpy as np
 import csv
 import rospkg
 import os
+import matplotlib.pyplot as plt  # only for plotting, not required for calculations
 
 rospy.init_node('allan_variance_node')
 
@@ -56,14 +57,19 @@ N = bag.get_message_count(topic) # number of measurement samples
 
 data = np.zeros( (6,N) ) # preallocate vector of measurements
 
+if isDeltaType:
+    scale = sampleRate
+else:
+    scale = 1.0
+
 cnt = 0
 for topic, msg, t in bag.read_messages(topics=[topic]):
-    data[0,cnt] = msg.linear_acceleration.x
-    data[1,cnt] = msg.linear_acceleration.y
-    data[2,cnt] = msg.linear_acceleration.z
-    data[3,cnt] = msg.angular_velocity.x
-    data[4,cnt] = msg.angular_velocity.y
-    data[5,cnt] = msg.angular_velocity.z
+    data[0,cnt] = msg.linear_acceleration.x * scale
+    data[1,cnt] = msg.linear_acceleration.y * scale
+    data[2,cnt] = msg.linear_acceleration.z * scale
+    data[3,cnt] = msg.angular_velocity.x * scale
+    data[4,cnt] = msg.angular_velocity.y * scale
+    data[5,cnt] = msg.angular_velocity.z * scale
     cnt = cnt + 1
 
 bag.close()
@@ -73,45 +79,70 @@ print "[%0.2f seconds] Bagfile parsed\n"%(rospy.get_time()-t0)
 """"""""""""""""""
 " Allan Variance "
 """"""""""""""""""
-if isDeltaType:
-    dtype = 'phase'
-else:
-    dtype = 'freq'
-
 if axis is 0:
     currentAxis = 1 # loop through all axes 1-6
 else:
     currentAxis = axis # just loop one time and break
 
 while (currentAxis <= 6):
-    (taus_used, adev, adeverror, adev_n) = allantools.oadev(data[currentAxis-1], data_type=dtype, rate=float(sampleRate), taus=np.array(taus) )
+    (taus_used, adev, adev_err, adev_n) = allantools.oadev(data[currentAxis-1], data_type='freq', rate=float(sampleRate), taus=np.array(taus) )
 
     """""""""""""""
     " Save as CSV "
     """""""""""""""
     if (currentAxis==1):
-        fname = 'allan_accel_x.csv'
+        fname = 'allan_accel_x'
+        title = 'Allan Deviation: Accelerometer X'
     elif (currentAxis==2):
-        fname = 'allan_accel_y.csv'
+        fname = 'allan_accel_y'
+        title = 'Allan Deviation: Accelerometer Y'
     elif (currentAxis==3):
-        fname = 'allan_accel_z.csv'
+        fname = 'allan_accel_z'
+        title = 'Allan Deviation: Accelerometer Z'
     elif (currentAxis==4):
-        fname = 'allan_gyro_x.csv'
+        fname = 'allan_gyro_x'
+        title = 'Allan Deviation: Gyroscope X'
     elif (currentAxis==5):
-        fname = 'allan_gyro_y.csv'
+        fname = 'allan_gyro_y'
+        title = 'Allan Deviation: Gyroscope Y'
     elif (currentAxis==6):
-        fname = 'allan_gyro_z.csv'
+        fname = 'allan_gyro_z'
+        title = 'Allan Deviation: Gyroscope Z'
 
     print "[%0.2f seconds] Finished calculating allan variance - writing results to %s"%(rospy.get_time()-t0,fname)
 
-    f = open(resultsPath + fname, 'wt')
+    f = open(resultsPath + fname + '.csv', 'wt')
 
     try:
         writer = csv.writer(f)
         writer.writerow( ('Tau', 'AllanDev', 'AllanDevError', 'AllanDevN') )
         for i in range(taus_used.size):
-            writer.writerow( (taus_used[i],adev[i],adeverror[i],adev_n[i])  )
+            writer.writerow( (taus_used[i],adev[i],adev_err[i],adev_n[i])  )
     finally:
         f.close()
 
+    """""""""""""""
+    " Plot Result "
+    """""""""""""""
+    plt.figure(figsize=(12,8))
+    ax = plt.gca()
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+
+    plt.plot(taus_used,adev)
+    plt.grid(True, which="both")
+    plt.title(title)
+    plt.xlabel('Tau (s)')
+    plt.ylabel('ADEV')
+
+    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(20)
+
+    plt.show(block=False)
+
+    plt.savefig(resultsPath + fname)
+
     currentAxis = currentAxis + 1 + axis*6 # increment currentAxis also break if axis is not =0
+
+inp=raw_input("Press Enter key to close figures and end program\n")
